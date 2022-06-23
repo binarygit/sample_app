f3360b8505861d910be460386f8e223c033cdf47
binarygit <inscription4@outlook.com>
2022-06-23 15:48:29 +0545

    Refactor full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,8 @@
 module ApplicationHelper
   def full_title(page_title = '')
     base_title = 'Ruby on Rails Tutorial Sample App'
-    #base_title if page_title.empty?
-    if page_title.empty?
-      base_title
-    else
-      page_title + ' | ' + base_title
-    end
+    return base_title if page_title.empty?
+
+    return page_title + ' | ' + base_title
   end
 end
