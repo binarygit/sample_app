d46cf6c025da822966c4b3b109cfe6c192ed4165
binarygit <inscription4@outlook.com>
2022-06-24 20:22:32 +0545

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+
+User.create!([
+  { name: 'Avi', email: 'avi@avi.com', password: 'foobar', password_confirmation: 'foobar'}
+])
