9216c024540f81dde1c00ca6357eea95ad77a245
binarygit <inscription4@outlook.com>
2022-06-25 08:55:25 +0545

    User can signup and write tests for signup functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      #redirect_to root_url
+      redirect_to @user
     else
       render :new, status: :unprocessable_entity
     end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,35 @@
+require "test_helper"
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "Error Messages are displayed when sign up attempt is unsuccessful" do
+    get signup_path
+    assert_response :success
+
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: 'Avi'} }
+    end
+    assert_response :unprocessable_entity
+    assert_template 'users/new'
+    assert_select '#error_explanation'
+  end
+
+  test "New user is created when sign up attemt is successful" do
+    get signup_path
+    assert_response :success
+
+    assert_difference 'User.count', 1 do
+      post users_path, params: {
+                          user: { 
+                            name: 'Avi',
+                            email: 'avi@avi.com',
+                            password: 'foobar',
+                            password_confirmation: 'foobar'
+                          } 
+                       }
+    end
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_template 'users/show'
+  end
+end
