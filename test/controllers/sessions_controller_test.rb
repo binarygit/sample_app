b8c5198584e17142b6062ef18b6e3915cd6c950e
binarygit <inscription4@outlook.com>
2022-06-25 20:18:14 +0545

    Create the login path and style login page

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get new" do
+    get login_path
+    assert_response :success
+    assert_select 'title', "Log in | #{ @base_title }"
+  end
+end
