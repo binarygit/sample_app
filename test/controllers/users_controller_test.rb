a7d7af7475ee7248970b66ad89a75b8124915ce2
binarygit <inscription4@outlook.com>
2022-06-25 06:27:25 +0545

    Create users#show, it's view and write test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,11 +3,17 @@ require "test_helper"
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
+    @user = User.create!(name: 'Avi', email: 'avi@avi.com', password: 'foobar', password_confirmation: 'foobar')
   end
 
   test "should get new" do
     get signup_path
     assert_response :success
     assert_select 'title', "Sign up | #{ @base_title }"
+  end
+
+  test "should get show" do
+    get user_path(@user)
+    assert_response :success
   end
 end
