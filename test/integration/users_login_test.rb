1852a9899b3a95cb190d3e84c1c55d962b246bbd
binarygit <inscription4@outlook.com>
2022-06-27 08:43:49 +0545

    Implement basic login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -27,16 +27,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
     post login_path, params: {
                        session: {
-                         email: 'avi@avi.com',
+                         email: @user.email,
                          password: ''
                        }
                      }
+    assert_not is_logged_in?
     assert_response :unprocessable_entity
     assert_template 'sessions/new'
     assert flash.any?
   end
 
-  test 'login with valid credentials' do
+  test 'login with valid credentials followed by logout' do
     get login_path
     assert_template 'sessions/new'
 
@@ -53,5 +54,12 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', login_path, count: 0
     assert_select 'a[href=?]', logout_path
     assert_select 'a[href=?]', user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
